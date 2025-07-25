fe00d05c3f8a527a9fa2993ed1850b61ba739a1d
Song He <songhe@hexaa.com>
2025-07-25 13:44:55 -0400

    Minor updated to allow InputFormat to support list[dict] as source. It helps to remove complaint from type checkers but has no runtime effects.

--- kafka_mocha/models/ktypes.py
+++ kafka_mocha/models/ktypes.py
@@ -2,4 +2,7 @@ from typing import Literal, TypeVar
 
 LogLevelType = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 OutputFormat = Literal["html", "csv"]
-InputFormat = dict[Literal["source", "topic", "serialize", "subject_name_strategy"], str | bool]
+InputFormat = dict[
+    Literal["source", "topic", "serialize", "subject_name_strategy"],
+    str | bool | list[dict],
+]
